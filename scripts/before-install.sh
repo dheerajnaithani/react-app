9af11a481ea1fb5ce8aa6d7ca1069727f08ba821
Dheeraj Naithani <dheeraj.naithani@gmail.com>
2021-03-09 23:53:44 -0500

    fix appspec.yml

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -3,4 +3,19 @@ mkdir -p /etc/nginx/site-available/xeniapp
 chmod 755 /etc/nginx/site-available/xeniapp
 
 mkdir -p /etc/nginx/site-enabled
-chmod 755
+chmod 755 /etc/nginx/site-enabled
+
+cat <<-EOF > /etc/nginx/site-enabled/xeniapp
+server {
+       listen 80;
+
+       root /opt/xeniuser;
+       index index.html;
+
+       location / {
+               try_files $uri $uri/ /index.html;
+       }
+}
+EOF
+
+chmod 755 /etc/nginx/site-enabled/xeniapp
